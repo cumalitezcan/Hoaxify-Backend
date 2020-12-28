4a6bfb3b0ce7d27d03c5937b000422507f8491c4
cumalitezcan <cumalitezcan@gmail.com>
2020-12-28 18:22:47 +0300

    crossOrigin

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,16 +1,14 @@
 package com.hoaxify.ws.user;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
 
 import lombok.Data;
 
-@Entity
+
 @Data
 public class User {
 	
-	@Id
-	private Long id;
+	
 	
 	private String username;
 	
