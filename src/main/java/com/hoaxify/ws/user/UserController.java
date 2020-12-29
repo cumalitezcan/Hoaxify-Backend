50bffb43c50b274f3f96c0d9121216b7ce349efe
cumalitezcan <cumalitezcan@gmail.com>
2020-12-29 11:53:40 +0300

    saveDataInDatabase

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,9 +1,9 @@
 package com.hoaxify.ws.user;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	//private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	@Autowired
+	UserRepository userRepository;
 
-	@CrossOrigin
 	@PostMapping("/api/1.0/users")
 	public void createUser(@RequestBody User user) {
 		
-		log.info(user.toString());
+		userRepository.save(user);
 		
 	}
 
